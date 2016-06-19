48e18e03b5df165aa2e7d214f3b17202aeaec6b7
songxunzhao <songxunzhao1991@gmail.com>
2016-06-19 01:04:41 -0500

    Update new version

---
+++ app/models/ContactRequest.php
@@ -0,0 +1,12 @@
+<?php 
+
+class ContactRequest extends \Illuminate\Database\Eloquent\Model
+{
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'contact_requests';
+}
---
+++ app/models/Faq.php
@@ -0,0 +1,12 @@
+<?php 
+
+class Faq extends \Illuminate\Database\Eloquent\Model
+{
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'faq';
+}
