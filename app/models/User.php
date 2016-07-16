448a3fc9b1491b5288c52078287786b56e15941b
songxunzhao <songxunzhao1991@gmail.com>
2016-07-16 11:00:08 +0800

    Changed user model

--- app/models/User.php
+++ app/models/User.php
@@ -10,7 +10,7 @@ class User extends \Illuminate\Database\Eloquent\Model
 	 */
     
 	protected $table = 'user';
-    protected $hidden = ['password'];
+    protected $hidden = ['password', 'created_at', 'updated_at'];
 
     public function influencer()
     {
