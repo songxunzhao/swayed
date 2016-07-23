03f22c238f55cfab3486337bfe5f79724e3bdeee
songxunzhao <songxunzhao1991@gmail.com>
2016-07-24 01:30:59 +0800

    Changed routes

--- src/routes.php
+++ src/routes.php
@@ -92,57 +92,57 @@ $app->add(function ($request, $response, $next) {
 
 // Routes
 
-$app->post('/v1/user/signup', 'App\Controller:signupUser');
+$app->post('/v1/user/signup', 'Controller:signupUser');
 
-$app->post('/v1/user/login', 'App\Controller:loginUser');
-
-
-$app->post('/v1/images', 'App\Controller:uploadImage');
-
-$app->post('/v1/campaigns', 'App\Controller:createCampaign');
-
-$app->get('/v1/contact_requests/{reqid}', 'App\Controller:getContactRequest');
+$app->post('/v1/user/login', 'App\Controller\Controller:loginUser');
 
 
-$app->post('/v1/contact_requests', 'App\Controller:createContactRequest');
+$app->post('/v1/images', 'App\Controller\Controller:uploadImage');
+
+$app->post('/v1/campaigns', 'App\Controller\Controller:createCampaign');
+
+$app->get('/v1/contact_requests/{reqid}', 'App\Controller\Controller:getContactRequest');
+
+
+$app->post('/v1/contact_requests', 'App\Controller\Controller:createContactRequest');
 
 
 
-$app->put('/v1/campaigns/ca{camid}', 'App\Controller:updateCampaign');
+$app->put('/v1/campaigns/ca{camid}', 'App\Controller\Controller:updateCampaign');
 
 
-$app->get('/v1/campaigns/{camid}', 'App\Controller:getCampaign');
+$app->get('/v1/campaigns/{camid}', 'App\Controller\Controller:getCampaign');
 
-$app->post('/v1/campaigns/list', 'App\Controller:getCampaignList');
+$app->post('/v1/campaigns/list', 'App\Controller\Controller:getCampaignList');
 
-$app->get('/v1/brand/campaigns', 'App\Controller:getBrandCampaignList');
+$app->get('/v1/brand/campaigns', 'App\Controller\Controller:getBrandCampaignList');
 
-$app->get('/v1/influencer/campaigns', 'App\Controller:getInfluencerCampaignContract');
-
-
-$app->post('/v1/user/profile/', 'App\Controller:getProfile');
+$app->get('/v1/influencer/campaigns', 'App\Controller\Controller:getInfluencerCampaignContract');
 
 
-$app->post('/v1/user/rate', 'App\Controller:updateUserRate');
+$app->post('/v1/user/profile/', 'App\Controller\Controller:getProfile');
 
-$app->post('/v1/user/interests', 'App\Controller:updateUserInterests');
 
-$app->get('/v1/interest_tags', 'App\Controller:getInterestTags');
+$app->post('/v1/user/rate', 'App\Controller\Controller:updateUserRate');
 
-$app->post('/v1/campaigns/{camid}/apply', 'App\Controller:applyForCampaign');
+$app->post('/v1/user/interests', 'App\Controller\Controller:updateUserInterests');
 
-$app->post('/v1/campaigns/{camid}/influencers/{influid}/offer', 'App\Controller:offerCampaign');
+$app->get('/v1/interest_tags', 'App\Controller\Controller:getInterestTags');
 
-$app->get('/v1/campaigns/{camid}/influencers', 'App\Controller:getContractsForCampaign');
+$app->post('/v1/campaigns/{camid}/apply', 'App\Controller\Controller:applyForCampaign');
 
-$app->get('/v1/campaigns/{camid}/cancel', 'App\Controller:cancelCampaign');
+$app->post('/v1/campaigns/{camid}/influencers/{influid}/offer', 'App\Controller\Controller:offerCampaign');
+
+$app->get('/v1/campaigns/{camid}/influencers', 'App\Controller\Controller:getContractsForCampaign');
+
+$app->get('/v1/campaigns/{camid}/cancel', 'App\Controller\Controller:cancelCampaign');
 
 $app->get('/v1/campaign_contracts/{contract_id}/{action:award|decline|accept|reject}',
-    'App\CampaignContractController:get');
+    'App\Controller\CampaignContractController:get');
 
-$app->post('/v1/influencers/list', 'App\Controller:searchInfluencerList');
+$app->post('/v1/influencers/list', 'App\Controller\Controller:searchInfluencerList');
 
-$app->get('/v1/faqs', 'Controller:getFaqList');
+$app->get('/v1/faqs', 'App\Controller\Controller:getFaqList');
 
 $app->get('/', function ($request, $response, $args) {
    	end:
